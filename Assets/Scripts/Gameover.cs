5823a8c80cbfe1722513f3b0a63bd90c5aea9469
syuuto <SDT2395GP0019@edu.sca.ac.jp>
2025-01-23 14:41:44 +0900

    エフェクト追加

--- Assets/Scripts/Gameover.cs
+++ Assets/Scripts/Gameover.cs
@@ -8,6 +8,7 @@ public class Gameover : MonoBehaviour
     [SerializeField] Material[] ColorSprite;  // 配列でマテリアルを指定
     private int HitCount = 0;  // 衝突回数のカウント
     [SerializeField] int Count;  // 破壊に必要な衝突回数
+    public GameObject effectPrefab;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -20,6 +21,11 @@ public class Gameover : MonoBehaviour
             // ColorSprite配列の次のマテリアルに変更
             int materialIndex = HitCount % ColorSprite.Length;
             GetComponent<Renderer>().material = ColorSprite[materialIndex];
+
+            // エフェクトの位置を衝突位置に設定
+            Vector2 collisionPoint = collision.contacts[0].point;
+            GameObject effect = Instantiate(effectPrefab, collisionPoint, Quaternion.identity);
+            
 
             // もし衝突回数が規定回数以上ならオブジェクトを破壊する
             if (HitCount > Count)
